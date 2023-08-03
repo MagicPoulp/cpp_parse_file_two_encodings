821f0f192846ab6fecc9c54c5979a38f917f5fef
Thierry Vilmart <thierry.vilmart@REPLACE.com>
2023-08-03 12:25:12 +0200

    first parsing of the input file

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 
-//wchar_t list[30];
-//fread( list, sizeof(wchar_t), 25, girl );
+using namespace std;
+
+// https://stackoverflow.com/questions/4059775/convert-iso-8859-1-strings-to-utf-8-in-c-c
+string latin1_to_utf8(string & ins) {
+  unsigned char *in, *out, *outStart;
+  int lenOut = ins.length();
+  out = new unsigned char[lenOut*2];
+  outStart = out;
+  in = reinterpret_cast<unsigned char*>(const_cast<char*>(ins.c_str()));
+  while (*in)
+    if (*in<128) *out++=*in++;
+    else *out++=0xc2+(*in>0xbf), *out++=(*in++&0x3f)+0x80;
+  std::string result(reinterpret_cast<char*>(outStart));
+  delete[](outStart);
+  return result;
+}
 
 int main(int argc, char* argv[])
 {
   if (argc != 2) {
-    fprintf(stderr, "Error: wrong number of arguments");
+    cerr << "Error: wrong number of arguments" << endl;
     return -1;
   }
-  printf("read %s\n", argv[1]);
+  ifstream newfile;
+  newfile.open(argv[1], ios::in);
+  if (newfile.is_open()) {
+    string tp;
+    while(getline(newfile, tp)){
+      std::string utf8_string = latin1_to_utf8(tp);
+      cout << utf8_string << "\n";
+    }
+    newfile.close();
+  }
   return 0;
 }
-
